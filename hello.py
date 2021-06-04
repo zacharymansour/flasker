0fd02283f7eef81fbb707c3da9c135ad92de6505
Zach Mansour <zacharyalanmansour@gmail.com>
2021-06-04 13:08:28 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#route decorators
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template("user.html", 
+		user_name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+		return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+		return render_template("500.html"), 500
